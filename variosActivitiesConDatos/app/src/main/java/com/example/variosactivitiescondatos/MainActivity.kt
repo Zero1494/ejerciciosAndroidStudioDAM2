09c315053ae12df5accb0d9b2328c5658dcc0aed
Carlos Ciudad <carlos.ciu94@gmail.com>
2023-10-09 18:08:26 +0200

    commit ventana 2 recreo

--- variosActivitiesConDatos/app/src/main/java/com/example/variosactivitiescondatos/MainActivity.kt
+++ variosActivitiesConDatos/app/src/main/java/com/example/variosactivitiescondatos/MainActivity.kt
@@ -1,13 +1,36 @@
 package com.example.variosactivitiescondatos
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.variosactivitiescondatos.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.boton.setOnClickListener {
+            irAVentana2()
+        }
+
     }
 
-    lateinit var amosmecagoendiosnometienesqueinvitaraunacerveza
+    private fun irAVentana2() {
+        //aqui se crea el objeto para cambiar de ventana
+        var miIntent = Intent(this,ventanaNumero2::class.java)
+        //Aqui podemos pasar los atributos creados en los elementos de texto que estan en
+        //nuestra primera ventana
+        //miIntent.putExtra("nombre",binding.cajaNombre.text.toString())
+        //miIntent.putExtra("edad",binding.cajaEdad.text.toString())
+        startActivity(miIntent)
+
+    }
+
+
 }
--- variosActivitiesConDatos/app/src/main/java/com/example/variosactivitiescondatos/ventanaNumero2.kt
+++ variosActivitiesConDatos/app/src/main/java/com/example/variosactivitiescondatos/ventanaNumero2.kt
@@ -8,4 +8,6 @@ class ventanaNumero2 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ventana_numero2)
     }
+
+
 }
